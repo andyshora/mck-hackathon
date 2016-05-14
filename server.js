8cffc4e3d3bada9b8940fc2c9a54d857f3161394
Andy Shora <andyshora@gmail.com>
2016-05-14 08:41:20 -0400

    Added paging hints

--- server.js
+++ server.js
@@ -8,24 +8,31 @@ app.use(express.static(__dirname));
 var options = { version: 2.2 };
 var context = new stackexchange(options);
 
-app.get('/api/questions/:tag', function(req, res) {
-  var filter = {
+function getRequestFilters(tag, page) {
+  return {
     key: config.key,
-    pagesize: 50,
-    tagged: req.params.tag,
+    pagesize: 100,
+    page: 1,
+    tagged: tag,
     sort: 'activity',
     order: 'asc'
   };
+}
+
+app.get('/api/questions/:tag', function(req, res) {
+
+  var filter = getRequestFilters(req.params.tag, 1);
 
   // Get all the questions (http://api.stackexchange.com/docs/questions) 
-  context.questions.questions(filter, function(err, results){
+  context.questions.questions(filter, function(err, results) {
     if (err) {
       res.status(500).send(err);
       console.error(err);
     }
 
     if (results.has_more) {
-      // recursively build results
+      // recursively build results before returning
+      // send another request with a 'page' parameter 1 more than last time
       console.log('There\'s more...');
     }
     
